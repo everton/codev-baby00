45be7e31d28ccd04531206faf101eb3e8ea75db1
Everton J. Carpes <everton.carpes@gmail.com>
2013-12-09 15:43:19 -0200

    Import da versao inicial projeto - Rails 4.0.2
      Ruby version              2.0.0 (x86_64-linux)
      RubyGems version          2.0.14
      Rack version              1.5
      Rails version             4.0.2
      JavaScript Runtime        Node.js (V8)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodevBaby00::Application.config.secret_key_base = 'f2cf904fe900e96f048edbc6e46a4de7f0e4220b1b02969bb3d646c9329b94b737d05892fd298ff3f564051625bd5ff155c318efc8c5d04beade6aee6c215232'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodevBaby00::Application.config.session_store :cookie_store, key: '_codev-baby00_session'
